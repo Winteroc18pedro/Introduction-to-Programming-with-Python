b33c8270b7f2eeae8e022bb73885bccca97e8d53
I_Was_Poisoned <82127238+Winteroc18pedro@users.noreply.github.com>
2023-12-30 18:25:07 +0000

    Add files via upload

---
+++ Indoor Voice.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 30 16:33:52 2023
+
+@author: pedro
+"""
+
+
+def say_something():
+    print(input("What do you want to say? ").lower())
+
+say_something()
+
+#. lower() is a built-in Python method primarily used for string handling. 
+#The . lower() method takes no arguments and returns the lowercased strings 
+#from the given string by converting each uppercase character to lowercase.
+
