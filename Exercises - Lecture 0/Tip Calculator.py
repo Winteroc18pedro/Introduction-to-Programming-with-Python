b5982b2168f2ebcbcc4d5ec3b217296d889e50c3
I_Was_Poisoned <82127238+Winteroc18pedro@users.noreply.github.com>
2024-01-11 16:42:25 +0000

    Add files via upload

---
+++ Exercises - Lecture 0/Tip Calculator.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 11 16:14:59 2024
+
+@author: pedro
+"""
+
+def cost_of_the_meal():
+    cost = float(input("How much did the meal cost? "))
+    return cost
+
+def percentage():
+    percent = int(input("How much would you like to tip in percentage? "))
+    return percent/100 #Decimal representation of percent.
+
+def main():
+    price = cost_of_the_meal()
+    tip_percentage = percentage()
+    total_tip = price * tip_percentage
+    print("The tip amount is ", end='')
+    print(round(total_tip, 1), "euros")
+    
+main()
+        
+
+
+
