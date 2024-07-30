c3f608c775fe0ba9e5a3ce915afa6bf67651c044
I_Was_Poisoned <82127238+Winteroc18pedro@users.noreply.github.com>
2024-07-30 10:50:33 +0100

    nutrition.py

--- Exercises - Lecture 2/nutrition.py
+++ Exercises - Lecture 2/nutrition.py
@@ -5,7 +5,7 @@ Created on Tue Jul 30 10:08:47 2024
 @author: NB30006
 """
 
-#Write a program that shows the nutrition facts for the 20 most popular 
+#Write a program that shows the nutrition facts for the 19 most popular 
 #fruits. It prompts the user to input a fruit and outputs the number of calories
 #in that fruit
 
