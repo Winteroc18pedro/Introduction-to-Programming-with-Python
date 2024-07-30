91e6537189c9fa48f5c783f367a601eb0852da1e
I_Was_Poisoned <82127238+Winteroc18pedro@users.noreply.github.com>
2024-07-30 10:49:46 +0100

    Add files via upload

---
+++ Exercises - Lecture 2/nutrition.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 30 10:08:47 2024
+
+@author: NB30006
+"""
+
+#Write a program that shows the nutrition facts for the 20 most popular 
+#fruits. It prompts the user to input a fruit and outputs the number of calories
+#in that fruit
+
+
+fruits = {"Apple":130,"Avocado":50,"Banana":110,"Cantaloupe":50,"Grapefruit":60,
+          "Grapes":90,"Kiwi":90, "Lemon":15,"Lime":20,"Nectarine":60,"Orange":80,
+          "Peach":60,"Pear":100,"Pineapple":50,"Plum":70,"Strawberry":50,
+          "Cherries":100,"Tangerine":50,"Watermelon":80}
+
+def get_calories(fruit):
+    if fruit.capitalize() in fruits:
+        print("Calories: ", fruits[fruit.capitalize()])
+    else:
+        print("That is not a fruit or it is misspelled")
+
+def main():
+    item = input("Type here the name of the fruit: ")
+    get_calories(item)
+    
+
+main()
