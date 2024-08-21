8f6f01808a7e3105bb8ab963984d6ebd7658e5d2
I_Was_Poisoned <82127238+Winteroc18pedro@users.noreply.github.com>
2024-08-21 16:38:56 +0100

    Add files via upload

---
+++ Exercises - Lecture 3/fuel_gauge.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 30 16:47:06 2024
+
+@author: NB30006
+"""
+
+#Write a program that prompts the user for a fraction formatted as X/Y wherein
+#each of the X and Y are integers and then outputs as a percentage rounded to
+#the nearest integer, how much fuel is in the tank. If 1% or less remains
+#the output is "E" to indicate that the tank is essentially empty, if 99% or more
+#remains the output is "F" to indicate that the tank is essentially full.
+#If X and Y are not integers,X is greater than Y or Y is 0, prompt the user
+#again
+
+def fraction(x, y):
+    return (x/y) * 100
+
+def main():
+    while True:
+        try:
+           x = int(input("What's the value of x?"))
+           y = int(input("What's the value of y?"))
+           if y == 0:
+               print("y cannot be zero")
+           elif x > y:
+               print("x cannot be greater than zero")
+               continue
+           percentage = round(fraction(x,y))
+           if percentage < 1:
+               print("E")
+           elif percentage > 99:
+               print("F")
+           else:
+               print(f"{percentage}%")
+           break
+        except ValueError:
+            print("That's not an integer. Try again")
+        
+
+        
+main()
+
+
